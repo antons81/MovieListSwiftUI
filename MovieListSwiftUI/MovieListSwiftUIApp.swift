ef5899fbc3074d1f5102203f2d086b9b588ded1a
Anton Stremovskiy <perlik@gmail.com>
2023-06-25 17:14:22 +0200

    Initial Commit

---
+++ MovieListSwiftUI/MovieListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListSwiftUIApp.swift
+//  MovieListSwiftUI
+//
+//  Created by Anton Stremovskiy on 25.06.23.
+//
+
+import SwiftUI
+
+@main
+struct MovieListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
